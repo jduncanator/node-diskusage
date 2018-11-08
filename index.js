a147689566f6f29b78d46f8886afe0d8f4e6d0ee
Jordon de Hoog <jordon.dehoog@gmail.com>
2018-11-07 19:56:19 -0500

    add promise support
    add demo
    add readme

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var native = require('./build/Release/diskusage.node');
+var native = require("./build/Release/diskusage.node");
 
 exports.check = function(path, callback) {
-    var result = undefined;
-    var error = undefined;
-    try {
-        result = native.getDiskUsage(path);
-    }
-    catch (error_) {
-        error = error_
-    }
-    callback(error, result);
+  if (callback) {
+    return check(path, callback);
+  }
+
+  return new Promise(function (resolve, reject) {
+      check(path, function (err, result) {
+          err ? reject(err) : resolve(result)
+      })
+  })
 };
 
 exports.checkSync = native.getDiskUsage;
+
+function check(path, callback) {
+  var result = undefined;
+  var error = undefined;
+
+  try {
+    result = native.getDiskUsage(path);
+  } catch (error_) {
+    error = error_;
+  }
+
+  callback(error, result);
+}
