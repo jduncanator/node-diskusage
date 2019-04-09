587fcf4ca7ceb6515f68787363f5d7d7ff83dfa2
jduncanator <jduncanator@users.noreply.github.com>
2019-04-09 10:45:31 +1000

    Update callback TypeScript definition

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,6 @@ type DiskUsage = {
     total: number;
 }
 
-export function check(path: string, callback: (error: Error, result: DiskUsage) => void): void;
+export function check(path: string, callback: (error?: Error, result?: DiskUsage) => void): void;
 export function check(path: string): Promise<DiskUsage>
 export function checkSync(path: string): DiskUsage;
