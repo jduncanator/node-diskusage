a302c2112f2057a1d9bb9b9d12f25e865ae70951
Jacob <jacobq@gmail.com>
2019-04-29 19:16:41 -0500

    fix: use Nan::Utf8String instead of v8::String::Utf8Value (#38)
    This fixes another incompatibility problem with node v12.0.0:
    
    ```
    node_modules\diskusage\src\main.cpp(26): error C2440: '<function-style-cast>': cannot convert from 'v8::Local<v8::Value>' to 'v8::String::Utf8Value' [C:\Users\User\Documents\tmp\node_modules\diskusage\build\diskusage.vcxproj]
      c:\users\User\documents\tmp\node_modules\diskusage\src\main.cpp(26): note: No constructor could take the source type, or constructor overload resolution was ambiguous
    ```

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ static NAN_METHOD(GetDiskUsage)
     Nan::HandleScope scope;
 
     try {
-        DiskUsage result = GetDiskUsage(*v8::String::Utf8Value(info[0]));
+        DiskUsage result = GetDiskUsage(*Nan::Utf8String(info[0]));
         info.GetReturnValue().Set(ConvertDiskUsage(result));
     }
     catch (const SystemError &error) {
